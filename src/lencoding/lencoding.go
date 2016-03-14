9543aed5777443150e15908f95bb05f5b8963b57
Meara <meara@52xue.org>
2016-03-14 19:50:55 +0800

    包分离

---
+++ src/lencoding/lencoding.go
@@ -0,0 +1,43 @@
+package lencoding
+
+import (
+	"io"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"bytes"
+	"io/ioutil"
+	"fmt"
+)
+
+func ToUTF8Reader(r io.Reader, charset string)(io.Reader){
+	switch charset {
+	case "gb2312", "GB2312":
+		return transform.NewReader(r, simplifiedchinese.HZGB2312.NewDecoder())
+	case "gbk", "GBK":
+		return transform.NewReader(r, simplifiedchinese.GBK.NewDecoder())
+	case "gb18030", "GB18030":
+		return transform.NewReader(r, simplifiedchinese.GB18030.NewDecoder())
+	default:
+		return r
+	}
+}
+func ToUTF8Byte(s *[]byte, charset string) ([]byte) {
+	I := bytes.NewReader(s)
+	var O io.Reader
+	switch charset {
+	case "gb2312", "GB2312":
+		O = transform.NewReader(I, simplifiedchinese.HZGB2312.NewDecoder())
+	case "gbk", "GBK":
+		O = transform.NewReader(I, simplifiedchinese.GBK.NewDecoder())
+	case "gb18030", "GB18030":
+		O = transform.NewReader(I, simplifiedchinese.GB18030.NewDecoder())
+	default:
+		return s
+	}
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		fmt.Println(e)
+		return s
+	}
+	return d
+}
--- src/main/httpClient.go
+++ src/main/httpClient.go
@@ -141,61 +141,7 @@ func Scrape() {
 /**
 
  */
-func httpDo(httpUrl string, data string, headers map[string]string, method string) (io.Reader) {
-	client := &http.Client{}
-	reg, _ := regexp.Compile(`<meta.+?charset=[^\w]?([-\w]+)`)
-	charset := "utf-8"
-	req, err := http.NewRequest(method, httpUrl, strings.NewReader(data))
-	if err != nil {
-		// handle error
-	}
-	if strings.EqualFold("POST", method) {
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	}
-	for k, v := range headers {
-		req.Header.Set(k, v)
-	}
-	//req.Header.Set("Cookie", "name=anny")
 
-	resp, err := client.Do(req)
-	rs := reg.FindSubmatch([]byte(resp.Header.Get("Content-Type")))
-	if len(rs) > 0 {
-		charset = string(rs[1])
-		return ToUTF8Reader(resp.Body,charset)
-	}
-	defer resp.Body.Close()
-
-
-	//缓冲读取
-	//buf := make([]byte, 1024)
-	//n, _ := resp.Body.Read(buf)
-	//rs := reg.FindSubmatch(buf)
-	//if len(rs) > 0 {
-	//	charset = string(rs[1])
-	//}
-	////
-	//fmt.Println("|", string(ToUTF8Byte(buf[:n],charset)), "|")
-	//for {
-	//	n, _ := resp.Body.Read(buf)
-	//	if 0 == n {
-	//		break
-	//	}
-	//
-	//	//f.WriteString(string(buf[:n]))
-	//	//fmt.Println(string(Decode(string(buf[:n]))))
-	//	fmt.Println(string(GBKToUTF8Byte(buf[:n],charset)))
-	//}
-
-
-	//直接读取
-	body, err := ioutil.ReadAll(resp.Body)
-	rs = reg.FindSubmatch(body)
-	if len(rs) > 0 {
-		charset = string(rs[1])
-	}
-	rsss := ToUTF8Byte(body, charset)
-	return bytes.NewReader(rsss);
-}
 func Decode(s []byte) (rs []byte) {
 	I := bytes.NewReader(s)
 	O := transform.NewReader(I, simplifiedchinese.GBK.NewDecoder())
